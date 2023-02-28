a2fb3a69d1434de7df3aaba6adf203eec187bbfa
HappySpitz <tatarinkristina2002@gmail.com>
2023-02-28 17:56:03 +0200

    main

---
+++ app.js
@@ -0,0 +1,18 @@
+// console.log('hello world');
+
+// -------------------------------------------
+
+// console.log(__dirname);
+// console.log(__filename);
+// console.log(process.cwd());
+
+// --------------------------------------------
+
+// const path = require('path')
+// console.log(path)
+
+// ---------------------------------------------
+
+// const os = require('os')
+// console.log(os.arch());
+// console.log(os.cpus());
